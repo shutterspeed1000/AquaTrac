636678e76a557f126bb859bb20e7b6c1d345e61f
Sean Worrell <spworrell@gmail.com>
2023-12-21 22:06:33 -0500

    router made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import Header from "./assets/component/header.jsx";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import "./index.css";
 
 function App() {
   return (
-    <>
+    <div className="bkgrd">
       <Header />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
